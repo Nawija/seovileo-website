2421c444b708c0059ff66d28e8472cda31c6e81a
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-31 11:46:59 +0100

    bg-main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 
-import HeroAnim from "@/public/hero-anim.json";
-import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import { LottieRefCurrentProps } from "lottie-react";
 import { useRef } from "react";
 
 export default function Home() {
   const lottieHeroAnimRef = useRef<LottieRefCurrentProps>(null);
+
+  const interactivity = {
+    mode: "scroll",
+    actions: [
+      {
+        visibility: [0, 0.2],
+        type: "stop",
+        frames: [0],
+      },
+      {
+        visibility: [0.2, 0.45],
+        type: "seek",
+        frames: [0, 45],
+      },
+      {
+        visibility: [0.45, 1.0],
+        type: "loop",
+        frames: [45, 60],
+      },
+    ],
+  };
+
   return (
     <div className="anim-opacity">
-      <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center lg:flex-row">
-        <div className="flex-c mx-auto flex flex-col items-center justify-center px-4 py-12 text-center lg:w-[72rem]">
-          <small className="mb-2 text-sm tracking-[0.5rem] text-black">
+      <div className="relative px-4 py-24 text-white">
+        <div className="absolute left-0 top-0 -z-10 h-full w-full overflow-hidden bg-[#00024C]">
+          <div className="absolute left-0 top-0 h-[40vw] w-[40vw] rounded-full bg-sky-400 blur-[80px] lg:-left-24 lg:-top-24 lg:h-[20vw] lg:w-[20vw] lg:blur-[103px]" />
+        </div>
+        <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center text-center ">
+          <small className="mb-2 text-sm tracking-[0.5rem]">
             WEB DEVELOPMENT
           </small>
           <h1 className="mx-auto max-w-screen-lg text-3xl font-medium lg:text-5xl">
@@ -30,11 +54,16 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className="w-full border bg-white flex-c relative">
-        <div className="w-full lg:w-[80%] mx-auto">
-          <Lottie lottieRef={lottieHeroAnimRef} animationData={HeroAnim} />
+      {/* <div className="flex-c relative w-full border bg-white">
+        <div className="relative h-80 w-full">
+          <Lottie
+            style={{ height: "500" }}
+            lottieRef={lottieHeroAnimRef}
+            animationData={HeroAnim}
+            interactivity={interactivity}
+          />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
