a48cca3f80de009097c781fc2fa7a7167a0c5f08
Valkon <96625039+Nawija@users.noreply.github.com>
2024-01-17 16:21:34 +0100

    gdfg

--- src/ui/background/CircleBlur.tsx
+++ src/ui/background/CircleBlur.tsx
@@ -3,7 +3,7 @@
 export default function CircleBlur() {
   return (
     <div className="absolute left-0 top-0 -z-10 h-full w-full overflow-hidden">
-      <div className="absolute left-0 top-0 h-[40vw] w-[40vw] rounded-full bg-orange-400 blur-[80px] lg:-left-24 lg:-top-24 lg:h-[15vw] lg:w-[15vw] lg:blur-[103px]" />
+      <div className="absolute slide-right left-0 top-0 h-[40vw] w-[40vw] rounded-full bg-orange-400 blur-[80px] lg:-left-24 lg:-top-24 lg:h-[15vw] lg:w-[15vw] lg:blur-[103px]" />
       <div className="absolute bottom-0 right-0 h-[40vw] w-[40vw] rounded-full bg-emerald-400 blur-[80px] lg:-bottom-24 lg:-right-24 lg:h-[20vw] lg:w-[20vw] lg:blur-[103px]" />
     </div>
   );
