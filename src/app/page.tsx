5b79f3c6fec42ad38f73177304428510cf5d25f6
Valkon <96625039+Nawija@users.noreply.github.com>
2024-01-20 13:05:23 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import uiux from "@/public/uiux.png"
 export default function Home() {
   return (
     <div className="anim-opacity relative mx-auto max-w-[2000px] ">
-      <section className="flex items-center justify-center flex-col relative md:flex-row">
+      <section className="flex items-center justify-center flex-col relative md:flex-row mt-10">
         <div className="z-20 flex max-w-screen-md flex-col items-start justify-center space-y-4 px-10 text-start">
           <h1 className="text-3xl sm:text-4xl lg:text-5xl font-medium text-main">Strony Internetowe</h1>
           <p>
@@ -19,14 +19,15 @@ export default function Home() {
           <Image
             src={uiux}
             alt="..."
-            className=" object-cover"
+            className="object-cover"
+            priority
           />
         </div>
       </section>
       <section className="mx-auto my-24 flex flex-col items-center justify-center space-x-4 md:flex-row lg:ml-24">
         <div className="mb-8 text-center font-semibold lg:ml-80 lg:text-start">
-          <small className="text-orange-900">Zobacz</small>
-          <p className="text-xl lg:text-2xl text-main">Najnowsze realizacje</p>
+          <small className="text-white bg-orange-700 px-2 py-1 font-light">Zobacz</small>
+          <p className="text-xl lg:text-2xl text-main mt-1">Najnowsze realizacje</p>
         </div>
         <div className="relative w-screen overflow-hidden">
           <CarouselPortfolio />
