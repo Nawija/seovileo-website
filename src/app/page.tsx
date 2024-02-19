0692c7d837c9ebedbe01c249c7c4aeca899aa583
Valkon <96625039+Nawija@users.noreply.github.com>
2024-02-19 19:01:10 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Hero from "../components/home/Hero";
-import Portfolio from "../components/home/Portfolio";
-import Test from "../components/home/Test";
+import React, { Suspense } from "react";
+const Hero = React.lazy(() => import("../components/home/Hero"));
+const Portfolio = React.lazy(() => import("../components/home/Portfolio"));
+const Test = React.lazy(() => import("../components/home/Test"));
 
 export default function Home() {
   return (
     <div className="anim-opacity relative mx-auto w-full overflow-hidden">
-      <Hero />
-      <Portfolio />
-      <Test />
+      <Suspense fallback={<div>Ładowanie...</div>}>
+        <Hero />
+      </Suspense>
+      <Suspense fallback={<div>Ładowanie...</div>}>
+        <Portfolio />
+      </Suspense>
+      <Suspense fallback={<div>Ładowanie...</div>}>
+        <Test />
+      </Suspense>
     </div>
   );
 }
