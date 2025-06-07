f61afcc6dc357cde8b1c7fe6f29c5194d896a2b0
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-06-07 14:43:45 +0200

    sdf

--- components/blog/BlogPopularComponent.tsx
+++ components/blog/BlogPopularComponent.tsx
@@ -16,7 +16,7 @@ export default function BlogPopularComponent({
   return (
     <Link
       href={`/blog/${slug}`}
-      className="bg-main anim-opacity border-main relative overflow-hidden rounded-lg border p-4 pb-12 "
+      className="bg-main anim-opacity border-main relative overflow-hidden rounded-lg border p-4 pb-12"
     >
       <div className="relative h-20 w-full lg:h-32">
         <Image
@@ -25,7 +25,7 @@ export default function BlogPopularComponent({
           alt={title}
           fill
         />
-        <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-black/70 to-transparent" />
+        <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black/70 to-transparent" />
       </div>
       <div className="flex flex-col items-start justify-center space-y-1 px-2 py-2 lg:space-y-2 lg:px-3 lg:py-3">
         <div className="text-smoke flex items-start justify-start">
@@ -37,19 +37,27 @@ export default function BlogPopularComponent({
           <small className="text-main">wiecej</small>
         </p>
       </div>
-      <p
-        className={clsx(
-          `absolute bottom-3 w-max rounded-full px-2 py-1 text-xs text-white`,
-          {
-            "bg-red-500": tags.includes("strony"),
-            "bg-pink-400": tags.includes("marketing"),
-            "bg-blue-400": tags.includes("technologia"),
-            "bg-violet-500": tags.includes("popularne"),
-          },
-        )}
-      >
-        {tags}
-      </p>
+      <div className="absolute bottom-3 flex gap-2">
+        {tags.map((tag) => (
+          <p
+            key={tag}
+            className={clsx("w-max rounded-full px-2 py-1 text-xs text-white", {
+              "bg-red-500": tag === "strony",
+              "bg-pink-400": tag === "marketing",
+              "bg-blue-400": tag === "technologia",
+              "bg-violet-500": tag === "popularne",
+              "bg-gray-500": ![
+                "strony",
+                "marketing",
+                "technologia",
+                "popularne",
+              ].includes(tag), // domyślna szarość
+            })}
+          >
+            {tag}
+          </p>
+        ))}
+      </div>
     </Link>
   );
 }
