de47f72264e876ca2020e85d7f8da7a6160b2738
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-06-07 14:13:41 +0200

    Update page.tsx

--- app/(routers)/blog/page.tsx
+++ app/(routers)/blog/page.tsx
@@ -1,16 +1,17 @@
-// page.tsx (główna strona bloga)
-import dynamic from "next/dynamic";
 import { getAllBlogPosts } from "@/lib/mdx";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const LogicReact = dynamic(() => import("./LogicReact"));
 
 const Blog = async () => {
   const mergedData = getAllBlogPosts();
-  
 
   return (
     <div className="anim-opacity">
-      <LogicReact mergedData={mergedData} />
+      <Suspense fallback={<div>Ładowanie wyników...</div>}>
+        <LogicReact mergedData={mergedData} />
+      </Suspense>
     </div>
   );
 };
