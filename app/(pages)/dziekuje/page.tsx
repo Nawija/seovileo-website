c73ad286b22ce3fa717716ef3befb1b3a5ccb54f
Valkon <96625039+Nawija@users.noreply.github.com>
2025-06-11 00:35:27 +0200

    Update page.tsx

--- app/(pages)/dziekuje/page.tsx
+++ app/(pages)/dziekuje/page.tsx
@@ -2,7 +2,6 @@
 
 import "@/app/(pages)/dziekuje/multicolor.css";
 import { MultiColorBtn } from "@/components/ui/buttons/MultiColorBtn";
-import { SecondBtn } from "@/components/ui/buttons/SecondBtn";
 import confetti from "canvas-confetti";
 import Link from "next/link";
 import { useEffect } from "react";
@@ -19,7 +18,7 @@ export default function DziekujePage() {
   return (
     <div className="anim-opacity flex min-h-[60vh] flex-col items-center justify-center px-4 text-center">
       <div className="w-full max-w-2xl space-y-6 p-1">
-        <h1 className="animate-multicolor text-5xl font-bold p-4">
+        <h1 className="animate-multicolor p-4 text-5xl font-bold">
           Dziękuje za wiadomość!
         </h1>
         <p className="text-lg text-white">
@@ -28,7 +27,9 @@ export default function DziekujePage() {
         </p>
 
         <Link href="/">
-          <MultiColorBtn className="px-4 py-2">Powrót na stronę główną </MultiColorBtn>
+          <MultiColorBtn className="px-4 py-2">
+            Powrót na stronę główną{" "}
+          </MultiColorBtn>
         </Link>
       </div>
     </div>
