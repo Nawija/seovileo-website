d5d6483a1f990b8478b39eb6545795311b2758f7
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-06-14 14:42:01 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Hero from "../components/home/Hero";
 const Portfolio = dynamic(() => import("../components/home/Portfolio"));
-const Carousel = dynamic(() => import("../components/home/Carousel"), {
-  loading: () => <div className="text-white">Ładowanie...</div>,
-});
 
 const pageTitle = "Seovileo - Audyty SEO, Strony Internetowe które Sprzedają";
 const pageDescription =
@@ -111,74 +108,164 @@ export default function Home() {
         </div>
       </section>
       <div className="bg-body relative h-[60vh]">
-        <section className="text-zinc-400 body-font overflow-hidden">
-  <div className="px-5 py-12 lg:py-24 mx-auto max-w-7xl">
-    <div className="flex flex-wrap -m-12">
-      <div className="p-12 md:w-1/2 flex flex-col items-start">
-        <span className="inline-block py-1 px-2 rounded bg-green-50 text-green-500 text-xs font-medium tracking-widest">CATEGORY</span>
-        <h2 className="sm:text-3xl text-2xl title-font font-medium text-white mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
-        <p className="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug you probably haven't heard of them hexagon kickstarter craft beer pork chic.</p>
-        <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-main mt-auto w-full">
-          <a className="text-green-500 inline-flex items-center">Learn More
-            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-          <span className="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-400">
-            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-              <circle cx="12" cy="12" r="3"></circle>
-            </svg>1.2K
-          </span>
-          <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-            </svg>6
-          </span>
-        </div>
-        <a className="inline-flex items-center">
-          <img alt="blog" src="https://dummyimage.com/104x104" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-          <span className="flex-grow flex flex-col pl-4">
-            <span className="title-font font-medium text-white">Holden Caulfield</span>
-            <span className="text-gray-400 text-xs tracking-widest mt-0.5">UI DEVELOPER</span>
-          </span>
-        </a>
-      </div>
-      <div className="p-12 md:w-1/2 flex flex-col items-start">
-        <span className="inline-block py-1 px-2 rounded bg-green-50 text-green-500 text-xs font-medium tracking-widest">CATEGORY</span>
-        <h2 className="sm:text-3xl text-2xl title-font font-medium text-white mt-4 mb-4">Pinterest DIY dreamcatcher gentrify single-origin coffee</h2>
-        <p className="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric.</p>
-        <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-main mt-auto w-full">
-          <a className="text-green-500 inline-flex items-center">Learn More
-            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-          <span className="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-400">
-            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-              <circle cx="12" cy="12" r="3"></circle>
-            </svg>1.2K
-          </span>
-          <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-            </svg>6
-          </span>
-        </div>
-        <a className="inline-flex items-center">
-          <img alt="blog" src="https://dummyimage.com/103x103" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-          <span className="flex-grow flex flex-col pl-4">
-            <span className="title-font font-medium text-white">Alper Kamu</span>
-            <span className="text-gray-400 text-xs tracking-widest mt-0.5">DESIGNER</span>
-          </span>
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
+        <section className="body-font overflow-hidden text-zinc-400">
+          <div className="mx-auto max-w-7xl px-5 py-12 lg:py-24">
+            <div className="-m-12 flex flex-wrap">
+              <div className="flex flex-col items-start p-12 md:w-1/2">
+                <span className="inline-block rounded bg-green-50 px-2 py-1 text-xs font-medium tracking-widest text-green-500">
+                  CATEGORY
+                </span>
+                <h2 className="title-font mt-4 mb-4 text-2xl font-medium text-white sm:text-3xl">
+                  Roof party normcore before they sold out, cornhole vape
+                </h2>
+                <p className="mb-8 leading-relaxed">
+                  Live-edge letterpress cliche, salvia fanny pack humblebrag
+                  narwhal portland. VHS man braid palo santo hoodie brunch trust
+                  fund. Bitters hashtag waistcoat fashion axe chia unicorn.
+                  Plaid fixie chambray 90s, slow-carb etsy tumeric. Cray pug you
+                  probably havent heard of them hexagon kickstarter craft beer
+                  pork chic.
+                </p>
+                <div className="border-main mt-auto mb-4 flex w-full flex-wrap items-center border-b-2 pb-4">
+                  <a className="inline-flex items-center text-green-500">
+                    Learn More
+                    <svg
+                      className="ml-2 h-4 w-4"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                  <span className="mr-3 ml-auto inline-flex items-center border-r-2 border-zinc-400 py-1 pr-3 text-sm leading-none text-gray-400">
+                    <svg
+                      className="mr-1 h-4 w-4"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                      <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    1.2K
+                  </span>
+                  <span className="inline-flex items-center text-sm leading-none text-gray-400">
+                    <svg
+                      className="mr-1 h-4 w-4"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                    6
+                  </span>
+                </div>
+                <a className="inline-flex items-center">
+                  <img
+                    alt="blog"
+                    src="https://dummyimage.com/104x104"
+                    className="h-12 w-12 flex-shrink-0 rounded-full object-cover object-center"
+                  />
+                  <span className="flex flex-grow flex-col pl-4">
+                    <span className="title-font font-medium text-white">
+                      Holden Caulfield
+                    </span>
+                    <span className="mt-0.5 text-xs tracking-widest text-gray-400">
+                      UI DEVELOPER
+                    </span>
+                  </span>
+                </a>
+              </div>
+              <div className="flex flex-col items-start p-12 md:w-1/2">
+                <span className="inline-block rounded bg-green-50 px-2 py-1 text-xs font-medium tracking-widest text-green-500">
+                  CATEGORY
+                </span>
+                <h2 className="title-font mt-4 mb-4 text-2xl font-medium text-white sm:text-3xl">
+                  Pinterest DIY dreamcatcher gentrify single-origin coffee
+                </h2>
+                <p className="mb-8 leading-relaxed">
+                  Live-edge letterpress cliche, salvia fanny pack humblebrag
+                  narwhal portland. VHS man braid palo santo hoodie brunch trust
+                  fund. Bitters hashtag waistcoat fashion axe chia unicorn.
+                  Plaid fixie chambray 90s, slow-carb etsy tumeric.
+                </p>
+                <div className="border-main mt-auto mb-4 flex w-full flex-wrap items-center border-b-2 pb-4">
+                  <a className="inline-flex items-center text-green-500">
+                    Learn More
+                    <svg
+                      className="ml-2 h-4 w-4"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                  <span className="mr-3 ml-auto inline-flex items-center border-r-2 border-zinc-400 py-1 pr-3 text-sm leading-none text-gray-400">
+                    <svg
+                      className="mr-1 h-4 w-4"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                      <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    1.2K
+                  </span>
+                  <span className="inline-flex items-center text-sm leading-none text-gray-400">
+                    <svg
+                      className="mr-1 h-4 w-4"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                    6
+                  </span>
+                </div>
+                <a className="inline-flex items-center">
+                  <img
+                    alt="blog"
+                    src="https://dummyimage.com/103x103"
+                    className="h-12 w-12 flex-shrink-0 rounded-full object-cover object-center"
+                  />
+                  <span className="flex flex-grow flex-col pl-4">
+                    <span className="title-font font-medium text-white">
+                      Alper Kamu
+                    </span>
+                    <span className="mt-0.5 text-xs tracking-widest text-gray-400">
+                      DESIGNER
+                    </span>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
       <section className="bg-main text-zinc-400">
         <div className="container mx-auto px-5 py-24">
@@ -189,7 +276,7 @@ export default function Home() {
             <p className="mx-auto text-base leading-relaxed lg:w-2/3">
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
               gentrify, subway tile poke farm-to-table. Franzen you probably
-              haven't heard of them man bun deep.
+              havent heard of them man bun deep.
             </p>
           </div>
           <div className="mx-auto flex w-full max-w-2xl flex-col items-end space-y-4 px-8 sm:flex-row sm:space-y-0 sm:space-x-4 sm:px-0 lg:w-2/3">
@@ -204,7 +291,7 @@ export default function Home() {
                 type="email"
                 id="email"
                 name="email"
-                className="bg-opacity-50 w-full rounded-lg px-3 py-0.5 border-main text-base leading-8 text-gray-700 ease-in-out bg-gray-100"
+                className="bg-opacity-50 border-main w-full rounded-lg bg-gray-100 px-3 py-0.5 text-base leading-8 text-gray-700 ease-in-out"
               />
             </div>
             <MainBtn className="w-40">Zapisz się</MainBtn>
